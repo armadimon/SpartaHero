0e86b602b075032bed4def1f1d719ece757bef87
woomay0608 <woohuck0608@naver.com>
2025-02-23 23:39:15 +0900

    [Feat] SKill Effect
    - Skill들의 정보를 담은 클래스 구현
    -버튼에 들어갈 메소드 구현

--- Assets/WH-Skill/Scripts/Skill.cs
+++ Assets/WH-Skill/Scripts/Skill.cs
@@ -5,6 +5,15 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+enum SkillType
+{ 
+    Default,
+    Melee,
+    Range,
+}
+
+
 public class Skill : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -17,7 +26,9 @@ public class Skill : MonoBehaviour
     private List<Image> Images = new List<Image>();
     private List<string> Skillname = new List<string>();
     private List<string> SKilldescrt = new List<string>();
-    void Start()
+    private List<SkillSet> Skills = new List<SkillSet>();
+
+    private void Awake()
     {
         for (int i = 0; i < transform.childCount; i++)
         {
@@ -27,7 +38,11 @@ public class Skill : MonoBehaviour
             SKilldescrt.Add(Buttons[i].transform.GetChild(2).GetComponent<string>());
         }
 
+    }
+    void Start()
+    {
         SkipBut.onClick.AddListener(Skip);
+        SkillCreate();
     }
 
     // Update is called once per frame
@@ -42,9 +57,84 @@ public class Skill : MonoBehaviour
         gameObject.SetActive(false);
     }
 
-    //
+    private void SkillSelcet()
+    {
+        
+       
+    }
 
+    private void SkillCreate()
+    {
+        Skills.Add(new SkillSet(SkillType.Default,0, "공격력 증가", "공격력이 증가해요", images[0], 5, AttackUp));
+        Skills.Add(new SkillSet(SkillType.Default,0, "공격력 증가", "공격력이 증가해요", images[0], 5, AttackUp));
+        Skills.Add(new SkillSet(SkillType.Default,0, "공격력 증가", "공격력이 증가해요", images[0], 5, AttackUp));
+        Skills.Add(new SkillSet(SkillType.Default,0, "공격력 증가", "공격력이 증가해요", images[0], 5, AttackUp));
+        Skills.Add(new SkillSet(SkillType.Default,0, "공격력 증가", "공격력이 증가해요", images[0], 5, AttackUp));
+        Skills.Add(new SkillSet(SkillType.Melee, 0,"공격력 증가", "공격력이 증가해요", images[0], 5, AttackSpeedUp));
+        Skills.Add(new SkillSet(SkillType.Melee, 0, "공격력 증가", "공격력이 증가해요", images[0], 5, AttackSpeedUp));
+        Skills.Add(new SkillSet(SkillType.Range, 0, "공격력 증가", "공격력이 증가해요", images[0], 5, AttackCountUp));
+        Skills.Add(new SkillSet(SkillType.Range, 0, "공격력 증가", "공격력이 증가해요", images[0], 5, AttackCountUp));
+    }
+
+    private void Cut()
+    {
+        foreach (SkillSet skill in Skills) 
+        {
+            if(skill.Level >= 3)
+            {
+                Skills.Remove(skill);
+            }
+        }
+    }
+
+    //
+    private void AttackUp()
+    {
+
+    }
+    private void AttackSpeedUp()
+    {
+
+    }
+    private void AttackRangeUp()
+    {
+
+    }
+    private void AttackDelayDown()
+    {
+
+    }
+    private void AttackCountUp()
+    {
+
+    }
+   
+
+    private class SkillSet
+    {
+        SkillType type;
+        public int Level;
+        string name;
+        string description;
+        Image Image;
+        float value;
+        Action Action;
+
+        public SkillSet(SkillType type,int Level, string name, string description, Image image, float value, Action action = null)
+        {
+            this.type = type;
+            this.Level = Level;
+            this.name = name;
+            this.description = description;
+            Image = image;
+            this.value = value;
+            Action = action;
+        }
+    }
 
 
 
 }
+
+
+
