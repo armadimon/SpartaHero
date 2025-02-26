ad0fab14f1f071c39c01ef6afc5cb62d67f666e1
S-SoHyun <slj506@naver.com>
2025-02-26 23:55:04 +0900

    [Feat] TitleScene 추가

--- Assets/SH_Scene/EnterDungeon.cs
+++ Assets/SH_Scene/EnterDungeon.cs
@@ -2,9 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EnterDungeon : MonoBehaviour
 {
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button exitButton;
+
+    private void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "TitleScene")
+        {
+            startButton.onClick.AddListener(OnClickStartButton);
+            exitButton.onClick.AddListener(OnclickExitButton);
+        }
+        else return;
+    }
+
+
+    void OnClickStartButton()
+    {
+        SceneManager.LoadScene("MainLobbyScene");
+    }
+
+    void OnclickExitButton()
+    {
+        Application.Quit();
+    }
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         SceneManager.LoadScene("StageScene");
