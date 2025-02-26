bd7983725b062c469ebc2bb0369c58b0cbb018c7
leesiyen <leesiyen02012@naver.com>
2025-02-26 16:48:06 +0900

    Auto Hit 추가

--- Assets/Scripts/Weapon/AutoHit.cs
+++ Assets/Scripts/Weapon/AutoHit.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Experimental.Video;
+using UnityEngine.UIElements;
 using static UnityEngine.GraphicsBuffer;
 
 
@@ -13,6 +16,8 @@ public class AutoHit : MonoBehaviour
 
     public LayerMask targetLayer;
     public Collider2D[] Enemy;
+    [SerializeField] private Transform projectileSpawnPosition;  //
+    private ProjectileManager projectileManager;
 
     public List<GameObject> targetsInRange = new List<GameObject>();
 
@@ -31,17 +36,34 @@ public class AutoHit : MonoBehaviour
             Vector3 mypos = transform.position;
             Vector3 targetPos = target.transform.position;
             float curDiff = Vector3.Distance(mypos, targetPos);
-            if (curDiff > diff)
+            if (curDiff < diff)
             {
                 diff = curDiff;
                 result = target.transform;
             }
+            
+
+
         }
         return result;
+    }
+    private void Start()
+    {
+        projectileManager = ProjectileManager.Instance;
+    }
 
+    private void CreateProjectile(Vector2 _lookDirection, float angle)
+    {
 
     }
-  
+    private static Vector2 RotateVector2(Vector2 v, float degree)
+    {
+        return Quaternion.Euler(0, 0, degree) * v;
+    }
 
 
+    public void Shot(float distance)
+    {
+        
+    }
 }
